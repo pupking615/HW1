d413e69ac152a70bc642e63416955187950c6a4a
pupking615 <41243120@nfu.edu.tw>
2024-10-22 23:19:51 +0800

    Add files via upload

---
+++ HW1/Ackermann's function.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct sf {
+    int m;
+    int n;
+    bool next; 
+};
+
+unsigned long ackermann(int m, int n) {
+    sf stack[1000]; // 定義堆疊的大小
+    int top = -1; // 堆疊的指標
+    unsigned long result = 0;
+
+    stack[++top] = { m, n, true }; // 定義初始狀態
+
+    while (top >= 0) 
+    {
+        sf frame = stack[top--];
+
+        m = frame.m;
+        n = frame.n;
+
+        if (m == 0) 
+        {
+            result = n + 1;
+        }
+        else if (m == 1) 
+        {
+            result = n + 2; 
+        }
+        else if (m == 2) 
+        {
+            result = 2 * n + 3; 
+        }
+        else if (m >= 3) 
+        {
+            if (n == 0) 
+            {
+                stack[++top] = { m - 1, 1, true }; 
+            }
+            else {
+                stack[++top] = { m - 1, -1, true }; // 記錄需要計算 A(m-1, A(m, n-1))
+                stack[++top] = { m, n - 1, true }; // 記錄 A(m, n - 1)
+                continue; 
+            }
+        }
+        if (frame.next) 
+        {
+            if (top >= 0) 
+            {
+                sf prevFrame = stack[top];
+                if (prevFrame.n == -1) 
+                {
+                    stack[top].n = result; // 設置結果為之前計算的結果
+                }
+            }
+        }
+    }
+
+    return result;
+S}
+
+int main() {
+    unsigned int m, n;
+    cout << "輸入 m 和 n：";
+    cin >> m >> n;
+
+    unsigned long result = ackermann(m, n); // 計算阿克曼函數
+    cout << "結果 = " << result << endl; // 輸出結果
+    return 0;
+}
+
+
+
+ 
---
+++ HW2/Problem 2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cstring> 
+
+using namespace std;
+
+void gp(char** set, int n, int index, char** cs, int setSize) {
+    if (index == n) {
+        std::cout << "{ ";
+        for (int i = 0; i < setSize; i++) {
+            std::cout << cs[i];
+            if (i < setSize - 1) {
+                std::cout << ", "; // 在元素之間添加逗號
+            }
+        }
+        std::cout << " }" << std::endl;
+        return;
+    }
+
+    // 不包含當前元素的情況
+    gp(set, n, index + 1, cs, setSize); 
+    // 包含當前元素的情況
+    cs[setSize] = set[index];
+    gp(set, n, index + 1, cs, setSize + 1);
+}
+
+int main() {
+    int ms;
+    cout << "請輸入集合成員數量: ";
+    cin >> ms;
+
+    char** set = new char* [ms]; // 動態分配字串陣列
+
+    // 輸入集合的成員
+    cout << "請輸入要產生冪集合的元素(請一個一個輸入):" << endl;
+    for (int i = 0; i < ms; i++) {
+        set[i] = new char[200]; // 假設每個字串的最大長度為199
+        cin >> set[i];
+    }
+
+    char** cs = new char* [ms]; // 動態分配當前子集的陣列
+    cout << "冪集合為:\n";
+    gp(set, ms, 0, cs, 0); // 開始產生冪集合
+}
